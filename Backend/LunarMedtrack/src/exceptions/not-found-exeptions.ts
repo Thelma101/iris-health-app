7aa9c957c2fa671f02c358e85b41c33e6a34a013
Thelma101 <akpata.thelma@gmail.com>
2026-01-18 08:39:09 +0100

    ensure user management responsivity and data fetch

--- Backend/LunarMedtrack/src/exceptions/not-found-exeptions.ts
+++
@@ -1,7 +0,0 @@
-import { ERRORCODES, HttpException } from "./root";
-
-export class NotFoundException extends HttpException {
-    constructor(message: string, errorCodes: ERRORCODES) {
-        super(message, errorCodes, 404, null);
-    }
-}
